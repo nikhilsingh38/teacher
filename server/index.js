0446b5348aa8b200b141f1c2f4ab42d062105204
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-04-17 13:49:09 +0530

    client server created

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://nikhil:nikhil_123@cluster0.58hrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log("Server running on port 5000"))
+  )
+  .catch((error) => console.log(error.message));
+
+
