b839a6e416d443de82a80556117f3b929b8fd0e7
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-04-17 13:55:28 +0530

    post url created

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,4 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get()
+router.get('/', (req, res) => {
+    res.send("hello")
+});
+
+export default router;
